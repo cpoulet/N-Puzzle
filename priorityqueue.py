06cfdb9d2c15850b1c383a8b5a7b62da60b30dc5
Florian VENTURINI <fventuri@student.42.fr>
2018-03-27 15:54:18 +0200

    [ADD] empty method + lenmax (and also header) in PriorityQueue

--- priorityqueue.py
+++ priorityqueue.py
@@ -1,3 +1,13 @@
+###############################################################################
+#            _   _        _____  _    _ _____________      ______             #
+#           | \ | |      |  __ \| |  | |___  /___  / |    |  ____|            #
+#           |  \| |______| |__) | |  | |  / /   / /| |    | |__               #
+#           | . ` |______|  ___/| |  | | / /   / / | |    |  __|              #
+#           | |\  |      | |    | |__| |/ /__ / /__| |____| |____             #
+#           |_| \_|      |_|     \____//_____/_____|______|______|            #
+#                                                                             #
+###############################################################################
+
 import heapq
 
 from state import State
@@ -7,11 +17,14 @@ class PriorityQueue:
         self._queue = []
         self._set   = set()
         self._index = 0
+        self.lenmax = 0
 
     def push(self, item, priority):
         heapq.heappush(self._queue, (priority, self._index, item))
         self._set.add(item)
         self._index += 1
+        if (self._index > self.lenmax):
+            self.lenmax = self._index
 
     def pop(self):
         _, item = heapq.heappop(self._queue)
@@ -19,13 +32,16 @@ class PriorityQueue:
         self._index -= 1
         return item
 
-    def contain(item):
+    def contain(self, item):
         return item in self._set
 
-    def remove(item):
+    def remove(self, item):
         self._set.remove(item)
         for i, elem in self._queue:
             if elem[1] is item:
                 del self._queue[i]
                 self._index -= 1
                 return
+
+    def empty(self):
+        return not len(self._queue)
