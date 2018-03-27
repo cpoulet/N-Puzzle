630e5c4ff9ffb6834685d93dcfccf99a73617216
Cedric Poulet <cpoulet@student.42.fr>
2018-03-27 17:51:03 +0200

    set -> dict

--- priorityqueue.py
+++ priorityqueue.py
@@ -15,28 +15,28 @@ from state import State
 class PriorityQueue:
     def __init__(self):
         self._queue = []
-        self._set   = set()
+        self._d     = {}
         self._index = 0
         self.lenmax = 0
 
     def push(self, item, priority):
         heapq.heappush(self._queue, (priority, self._index, item))
-        self._set.add(item)
+        self._d[item] = item
         self._index += 1
         if (self._index > self.lenmax):
             self.lenmax = self._index
 
     def pop(self):
         _, _, item = heapq.heappop(self._queue)
-        self._set.remove(item)
+        del self._d[item]
         self._index -= 1
         return item
 
     def __contains__(self, item):
-        return item in self._set
+        return item in self._d
 
     def remove(self, item):
-        self._set.remove(item)
+        del self._d[item]
         for i, elem in self._queue:
             if elem[2] is item:
                 del self._queue[i]
