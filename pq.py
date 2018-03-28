e81aef9a48212cd25dc05eef768ee6ae6cb4c8e1
Florian VENTURINI <fventuri@student.42.fr>
2018-03-28 18:27:41 +0200

    [EDIT] Changed name OrderedDictionnary -> PriorityQueue

--- pq.py
+++ pq.py
@@ -10,38 +10,35 @@
 
 import heapq
 
-from state import State
-
 class PriorityQueue:
+    '''Sorted Dictionnary with:
+    O(1) :      pop()
+                contain()
+    O(log(n)) : insort()'''
     def __init__(self):
+        self._d = {}
         self._queue = []
-        self._d     = {}
-        self._index = 0
         self.lenmax = 0
 
-    def push(self, item, priority):
-        heapq.heappush(self._queue, (priority, self._index, item))
-        self._d[item] = item
-        self._index += 1
-        if (self._index > self.lenmax):
-            self.lenmax = self._index
+    def push(self, key, value, sort_val):
+        self._d[key] = value
+        heapq.heappush(self._queue, (sort_val, key))
+        if len(self._queue) > self.lenmax:
+            self.lenmax += 1
+
+    def __contains__(self, key):
+        return key in self._d
+
+    def get(self, key):
+        return self._d.get(key)
 
     def pop(self):
-        _, _, item = heapq.heappop(self._queue)
-        del self._d[item]
-        self._index -= 1
-        return item
+        _, key = heapq.heappop(self._queue)
+        return self._d.pop(key)
 
-    def __contains__(self, item):
-        return item in self._d
-
-    def remove(self, item):
-        del self._d[item]
-        for i, elem in self._queue:
-            if elem[2] is item:
-                del self._queue[i]
-                self._index -= 1
-                return
+    def remove(self, key):
+        state = self._d.pop(key)
+        self._queue.remove((state.f, key))
 
     def empty(self):
         return not len(self._queue)
